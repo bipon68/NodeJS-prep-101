86ceccf12b5bae775edff1aeed42b31b32f2b115
bipon68 <bipon68@users.noreply.github.com>
2023-05-08 23:42:16 +0600

    #3 ticket model

---
+++ raffle-draw-project/src/Ticket.js
@@ -0,0 +1,18 @@
+const shortid = requir('shortid');
+
+class Ticket{
+/**
+ * Ticket constructor will recevie username and price
+ * @param {string} username
+ * @param {number} price
+ */
+    constructor(username, price){
+        this.id = shortid.generate();
+        this.username = username;
+        this.price = price;
+        this.createdAt = new Date();
+        this.updatedAt = new Date();
+    }
+}
+
+module.exports = Ticket;
